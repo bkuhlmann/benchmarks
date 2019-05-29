51e4fc02eec49a1f88112c6bada4b620bcc266e2
Brooke Kuhlmann <brooke@alchemists.io>
2019-05-29 07:12:24 -0600

    Added method proc benchmark.
    Benchmarks the speed different between a direct and proc method
    message. The latter form becomes interesting when using a functional
    style.

---
+++ scripts/ruby/methods/method_proc.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "benchmark/ips"
+
+Example = Class.new do
+  WORD = "hello"
+
+  def self.direct_say
+    say WORD
+  end
+
+  def self.proc_say
+    method(:say).call WORD
+  end
+
+  def self.say word
+    "Example says: #{word}."
+  end
+end
+
+Benchmark.ips do |benchmark|
+  benchmark.config time: 5, warmup: 2
+
+  benchmark.report "Direct" do
+    Example.direct_say
+  end
+
+  benchmark.report "Proc" do
+    Example.proc_say
+  end
+
+  benchmark.compare!
+end
+
+__END__
+
+Warming up --------------------------------------
+              Direct   239.276k i/100ms
+                Proc   140.720k i/100ms
+Calculating -------------------------------------
+              Direct      5.307M (± 6.0%) i/s -     26.560M in   5.025382s
+                Proc      2.033M (± 6.5%) i/s -     10.132M in   5.005368s
+
+Comparison:
+              Direct:  5306996.8 i/s
+                Proc:  2033254.9 i/s - 2.61x  slower
