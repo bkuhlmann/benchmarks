c8e2d82c1348ae92fa8fdbea19475914d3f30624
Brooke Kuhlmann <brooke@alchemists.io>
2020-05-07 08:31:08 -0600

    Added duplicate hash merge benchmark
    The subtly of this benchmark is showing the performance cost when
    dealing with a frozen hash which needs to be merged with another hash.
    In that situation, you'd need to dup the frozen hash or have the frozen
    hash merged with a new hash. This benchmark highlights the cost in that
    approach where using a reverse merge or a splat would be more
    performant.

--- scripts/ruby/hashes/merge.rb
+++ scripts/ruby/hashes/merge.rb
@@ -3,21 +3,25 @@
 require "bundler/setup"
 require "benchmark/ips"
 
-example = {b: 2}
+extra = {b: 2}
 
 Benchmark.ips do |benchmark|
   benchmark.config time: 5, warmup: 2
 
   benchmark.report "Splat" do
-    {a: 1, **example}
+    {a: 1, **extra}
   end
 
   benchmark.report "Merge" do
-    {a: 1}.merge example
+    {a: 1}.merge extra
   end
 
   benchmark.report "Merge!" do
-    {a: 1}.merge! example
+    {a: 1}.merge! extra
+  end
+
+  benchmark.report "Dup Merge!" do
+    {a: 1}.dup.merge! extra
   end
 
   benchmark.compare!
@@ -26,15 +30,18 @@ end
 __END__
 
 Warming up --------------------------------------
-               Splat   169.554k i/100ms
-               Merge   134.537k i/100ms
-              Merge!   188.759k i/100ms
+               Splat   144.638k i/100ms
+               Merge   123.591k i/100ms
+              Merge!   171.197k i/100ms
+          Dup Merge!    98.779k i/100ms
 Calculating -------------------------------------
-               Splat      2.937M (±27.7%) i/s -     13.903M in   5.040145s
-               Merge      1.866M (±20.1%) i/s -      9.149M in   5.058798s
-              Merge!      3.031M (±23.3%) i/s -     14.534M in   5.014792s
+               Splat      2.738M (±15.2%) i/s -     13.451M in   5.024280s
+               Merge      1.772M (±25.1%) i/s -      8.651M in   5.090233s
+              Merge!      2.795M (±13.3%) i/s -     13.867M in   5.051995s
+          Dup Merge!      1.448M (±16.9%) i/s -      7.112M in   5.004984s
 
 Comparison:
-              Merge!:  3031078.9 i/s
-               Splat:  2936693.7 i/s - same-ish: difference falls within error
-               Merge:  1866417.7 i/s - 1.62x  slower
+              Merge!:  2795412.8 i/s
+               Splat:  2738338.0 i/s - same-ish: difference falls within error
+               Merge:  1771976.9 i/s - 1.58x  slower
+          Dup Merge!:  1447942.4 i/s - 1.93x  slower
