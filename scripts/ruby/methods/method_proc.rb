61bd8d3afcda32ec8ab15fa372d85b7dab081652
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:31:25 -0700

    Refactored method proc benchmark methods
    Necessary to use improved descriptive and short names. The benchmark
    results were updated as a result as a double check.

--- scripts/ruby/methods/method_proc.rb
+++ scripts/ruby/methods/method_proc.rb
@@ -9,27 +9,27 @@ Example = Class.new do
     @first_word = words.first
   end
 
-  def single_direct_say
+  def direct_single
     say first_word
   end
 
-  def multi_direct_say
+  def direct_multiple
     words.each { |word| say word }
   end
 
-  def single_proc_say
+  def proc_single
     method(:say).call first_word
   end
 
-  def multi_proc_say
+  def proc_multiple
     words.each { |word| method(:say).call word }
   end
 
-  def single_method_to_proc_say
+  def method_to_proc_single
     first_word.then(&method(:say))
   end
 
-  def multi_method_to_proc_say
+  def method_to_proc_multiple
     words.each(&method(:say))
   end
 
@@ -48,27 +48,27 @@ Benchmark.ips do |benchmark|
   benchmark.config time: 5, warmup: 2
 
   benchmark.report "Direct (s)" do
-    example.single_direct_say
+    example.direct_single
   end
 
   benchmark.report "Direct (m)" do
-    example.multi_direct_say
+    example.direct_multiple
   end
 
   benchmark.report "Proc (s)" do
-    example.single_proc_say
+    example.proc_single
   end
 
   benchmark.report "Proc (m)" do
-    example.multi_proc_say
+    example.proc_multiple
   end
 
   benchmark.report "Method To Proc (s)" do
-    example.single_method_to_proc_say
+    example.method_to_proc_single
   end
 
   benchmark.report "Method To Proc (m)" do
-    example.multi_method_to_proc_say
+    example.method_to_proc_multiple
   end
 
   benchmark.compare!
@@ -77,24 +77,24 @@ end
 __END__
 
 Warming up --------------------------------------
-          Direct (s)   737.120k i/100ms
-          Direct (m)   267.719k i/100ms
-            Proc (s)   349.044k i/100ms
-            Proc (m)   122.588k i/100ms
-  Method To Proc (s)   185.665k i/100ms
-  Method To Proc (m)   137.666k i/100ms
+          Direct (s)   640.887k i/100ms
+          Direct (m)   232.071k i/100ms
+            Proc (s)   345.925k i/100ms
+            Proc (m)   124.249k i/100ms
+  Method To Proc (s)   208.061k i/100ms
+  Method To Proc (m)   137.101k i/100ms
 Calculating -------------------------------------
-          Direct (s)      7.391M (± 1.0%) i/s -     37.593M in   5.087103s
-          Direct (m)      2.666M (± 1.0%) i/s -     13.386M in   5.021959s
-            Proc (s)      3.445M (± 2.5%) i/s -     17.452M in   5.068574s
-            Proc (m)      1.200M (± 2.6%) i/s -      6.007M in   5.010571s
-  Method To Proc (s)      1.867M (± 3.5%) i/s -      9.469M in   5.077309s
-  Method To Proc (m)      1.339M (± 3.0%) i/s -      6.746M in   5.040639s
+          Direct (s)      6.414M (± 0.5%) i/s -     32.685M in   5.096301s
+          Direct (m)      2.319M (± 0.3%) i/s -     11.604M in   5.003526s
+            Proc (s)      3.444M (± 1.3%) i/s -     17.296M in   5.022878s
+            Proc (m)      1.225M (± 2.0%) i/s -      6.212M in   5.074410s
+  Method To Proc (s)      2.042M (± 1.7%) i/s -     10.403M in   5.095877s
+  Method To Proc (m)      1.371M (± 1.5%) i/s -      6.855M in   4.999989s
 
 Comparison:
-          Direct (s):  7390579.1 i/s
-            Proc (s):  3445035.9 i/s - 2.15x  (± 0.00) slower
-          Direct (m):  2665754.8 i/s - 2.77x  (± 0.00) slower
-  Method To Proc (s):  1867035.7 i/s - 3.96x  (± 0.00) slower
-  Method To Proc (m):  1339351.2 i/s - 5.52x  (± 0.00) slower
-            Proc (m):  1199565.8 i/s - 6.16x  (± 0.00) slower
+          Direct (s):  6413657.8 i/s
+            Proc (s):  3444066.2 i/s - 1.86x  (± 0.00) slower
+          Direct (m):  2319091.6 i/s - 2.77x  (± 0.00) slower
+  Method To Proc (s):  2042021.8 i/s - 3.14x  (± 0.00) slower
+  Method To Proc (m):  1371322.2 i/s - 4.68x  (± 0.00) slower
+            Proc (m):  1224757.8 i/s - 5.24x  (± 0.00) slower
