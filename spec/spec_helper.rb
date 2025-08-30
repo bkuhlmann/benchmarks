4302b76c49fe32a91e20fea211e46264cd570a9e
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-30 16:05:36 -0600

    Updated RSpec configuration to be consistent with other projects
    This was a little dated so have updated to reflect link:https://alchemists.io/projects/rubysmith[Rubysmith] defaults.
    
    Milestone: patch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,11 +22,10 @@ Pathname.require_tree SPEC_ROOT.join("support/shared_contexts")
 RSpec.configure do |config|
   config.color = true
   config.disable_monkey_patching!
-  config.example_status_persistence_file_path = "./tmp/rspec-status.txt"
+  config.example_status_persistence_file_path = "./tmp/rspec-examples.txt"
   config.filter_run_when_matching :focus
-  config.formatter = ENV.fetch("CI", false) == "true" ? :progress : :documentation
-  config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
-  config.order = "random"
+  config.formatter = ENV.fetch("CI", nil) == "true" ? :progress : :documentation
+  config.order = :random
   config.pending_failure_output = :no_backtrace
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.warnings = true
@@ -35,4 +34,11 @@ RSpec.configure do |config|
     expectations.syntax = :expect
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+  end
+
+  Kernel.srand config.seed
 end
